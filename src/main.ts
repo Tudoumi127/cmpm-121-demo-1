defd3956dded48877a76abd43819649ab41dde24
Tudoumi127 <123220803+Tudoumi127@users.noreply.github.com>
2024-10-07 16:52:53 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,10 @@ const gameName = "I am so confused rn you dont even know";
 document.title = gameName;
 
 const header = document.createElement("h1");
+const button = document.createElement("button");
+
+button.textContent = "🗿";
+
 header.innerHTML = gameName;
 app.append(header);
+app.append(button);
