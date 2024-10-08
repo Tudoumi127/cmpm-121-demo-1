f8a118ca8d55a5015e75a5f4abf4eb3f68da5aef
Tudoumi127 <123220803+Tudoumi127@users.noreply.github.com>
2024-10-07 18:24:52 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,21 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "I am so confused rn you dont even know";
+const gameName = "I am so Confused rn You Dont Even Know";
 document.title = gameName;
 
 let moais: number = 0;
+let moaiUpgrades: number = 0;
 
 const header = document.createElement("h1");
 const button = document.createElement("button");
+const purchase = document.createElement("button");
 const division = document.createElement("div");
+const upgrades = document.createElement("div");
 let previousTime = 0;
 
 button.textContent = "🗿";
+purchase.textContent = "Maimaimai (costs 10 moais)";
 
 /*
 setInterval(() => {
@@ -22,13 +26,18 @@ setInterval(() => {
 }, 1000);
 */
 
+function updateButton() {
+    purchase.disabled = moais < 10;
+}
+
 function update(timestamp: number) {
   // Calculate the time difference since the last recorded time
   const elapsed = timestamp - previousTime;
 
-  if (elapsed >= 1000) {
+  if (elapsed >= (1000/moaiUpgrades)) {
     moais++;
     division.textContent = `${moais} moais`;
+    updateButton();
 
     // Reset the previousTime to the current timestamp
     previousTime = timestamp;
@@ -42,11 +51,27 @@ button.addEventListener("click", () => {
   //console.log('aaaaaaaaa');
   moais++;
   division.textContent = `${moais} moais`;
+  updateButton();
 });
+
+purchase.addEventListener("click",() => {
+    moais -= 10;
+    updateButton();
+    moaiUpgrades++;
+    if(moaiUpgrades >= 1){
+        requestAnimationFrame(update);
+    }
+    division.textContent = `${moais} moais`;
+    upgrades.textContent = `${moaiUpgrades} moai upgrade(s)`;
+})
 
 header.innerHTML = gameName;
 
+updateButton();
 app.append(header);
 app.append(button);
+app.append(purchase);
 app.append(division);
-requestAnimationFrame(update);
+app.append(upgrades);
+
+
