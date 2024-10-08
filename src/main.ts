b2245d38ebdd423adb4a3e35c4c87cadbe6b53c6
Tudoumi127 <123220803+Tudoumi127@users.noreply.github.com>
2024-10-07 17:11:11 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,23 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "I am so confused rn you dont even know";
 document.title = gameName;
 
+let moais: number = 0;
+
 const header = document.createElement("h1");
 const button = document.createElement("button");
+const division = document.createElement("div");
 
 button.textContent = "🗿";
 
+button.addEventListener("click", () => {
+    //console.log('aaaaaaaaa');
+    moais++;
+    division.textContent = `${moais} moyais`;
+});
+
+
 header.innerHTML = gameName;
+
 app.append(header);
 app.append(button);
+app.append(division);
